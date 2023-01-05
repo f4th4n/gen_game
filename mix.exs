d4edbc58a7b09f2a06794f6d1355853d318463cf
Wildan <wilfathan@gmail.com>
2023-01-05 20:36:41 +0700

    Update mix

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Jumpa.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
+      {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
@@ -43,7 +43,7 @@ defmodule Jumpa.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.4"},
       {:plug_cowboy, "~> 2.5"},
       {:cors_plug, "~> 2.0"},
       {:distillery, "~> 2.1"}
