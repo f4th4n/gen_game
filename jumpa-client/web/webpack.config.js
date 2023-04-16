e52173e79b5d41263e4e67f616b8395c0b71ba8a
Wildan Fathan <wildan.nugraha@coinbit.id>
2023-04-16 14:35:47 +0700

    Add iex --remsh jumpa --sname dev

--- jumpa-client/web/webpack.config.js
+++ jumpa-client/web/webpack.config.js
@@ -1,27 +1,25 @@
 const path = require('path')
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 	devtool: 'eval-source-map',
 	watchOptions: {
 		ignored: /node_modules/,
 	},
 	plugins: [
 		new BrowserSyncPlugin({
-			files: [
-				'./src/**/*.js'
-			],
+			files: ['./src/**/*.js'],
 			watchTask: true,
-			open: "external",
-			logLevel: "silent",
-      host: 'localhost',
-      port: 3000,
-			server: { baseDir: ['.'] }
+			open: 'external',
+			logLevel: 'silent',
+			host: '0.0.0.0',
+			port: 3000,
+			server: { baseDir: ['.'] },
 		}),
-	]
+	],
 }
