2661dee664465eac5b3479cf3211920574e03ff7
Wildan Fathan <wildan.nugraha@coinbit.id>
2023-04-25 19:35:36 +0700

    Fix wrong db in test environment

--- jumpa-api/config/runtime.exs
+++ jumpa-api/config/runtime.exs
@@ -8,7 +8,11 @@ import Config
 # The block below contains prod specific runtime configuration.
 
 database_url =
-  System.get_env("DATABASE_URL") || "postgres://postgres:postgres@localhost:5433/jumpa_dev"
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
 config :jumpa_api, Jumpa.Repo,
   # ssl: true,
@@ -17,7 +21,6 @@ config :jumpa_api, Jumpa.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 if config_env() == :prod do
-
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
