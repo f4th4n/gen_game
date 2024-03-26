cf3a58772eda812b537f3f71ab8b7d902e112655
Wildan Fathan <wildan.nugraha@coinbit.id>
2024-03-26 13:23:58 +0700

    Init game socket

---
+++ lib/gen_game_web/channels/room_channel.ex
@@ -0,0 +1,30 @@
+defmodule GenGameWeb.RoomChannel do
+  use GenGameWeb, :channel
+
+  @impl true
+  def join("room:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  @impl true
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (room:lobby).
+  @impl true
+  def handle_in("shout", payload, socket) do
+    broadcast(socket, "shout", payload)
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
--- lib/gen_game_web/endpoint.ex
+++ lib/gen_game_web/endpoint.ex
@@ -13,6 +13,10 @@ defmodule GenGameWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  socket "/game", GenGameWeb.GameSocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
