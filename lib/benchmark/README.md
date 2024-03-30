bf82666fd1d9ccb3d774979d038a678681980c90
Wildan Fathan <wildan.nugraha@coinbit.id>
2024-03-31 01:03:08 +0700

    Finish benchmark module

--- lib/benchmark/README.md
+++ lib/benchmark/README.md
@@ -5,3 +5,16 @@
 1. Number of CCU
 2. Register new account
 3. Gameplay
+
+## Setup
+
+```bash
+sysctl -w fs.file-max=12000500
+sysctl -w fs.nr_open=20000500
+ulimit -n 20000000
+sysctl -w net.ipv4.tcp_mem='10000000 10000000 10000000'
+sysctl -w net.ipv4.tcp_rmem='1024 4096 16384'
+sysctl -w net.ipv4.tcp_wmem='1024 4096 16384'
+sysctl -w net.core.rmem_max=16384
+sysctl -w net.core.wmem_max=16384
+```
--- lib/benchmark/benchmark.ex
+++ lib/benchmark/benchmark.ex
@@ -1,15 +1,82 @@
 defmodule Benchmark.Benchmark do
-  def start() do
+  @moduledoc """
+  To collect cpu and memory usage. Use together with Tsung.
+  """
+  use Agent
+
+  require Logger
+
+  def start_benchmark() do
+    IO.puts(
+      "start collecting cpu & memory usage...\nto stop, call Benchmark.Benchmark.stop_benchmark()"
+    )
+
+    ticker = Task.async(&tick/0)
+    Agent.start_link(fn -> %{ticker: ticker, usages: []} end, name: __MODULE__)
+    :ok
   end
 
-  def print_cpu_usage(interval_in_s) do
+  def stop_benchmark() do
+    %{usages: usages, ticker: ticker} = Agent.get(__MODULE__, & &1)
+    benchmark_file = "/tmp/gen_game_benchmark_" <> Ecto.UUID.generate()
+    File.write!(benchmark_file, Enum.join(usages, "\n"))
+    IO.puts("write resource usages to #{benchmark_file}")
+    nil = Task.shutdown(ticker)
+    Agent.stop(__MODULE__)
   end
 
-  def cpu_usage() do
-    ~s[grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}']
+  def tick() do
+    %{
+      memory_usage: memory_usage,
+      memory_usage_unit: memory_usage_unit,
+      cpu_load: cpu_load,
+      local_time: local_time
+    } = get_resource_usage()
+
+    resource_data =
+      "[#{local_time}] cpu load (1m): #{cpu_load}, memory: #{memory_usage} #{memory_usage_unit}"
+
+    Agent.update(__MODULE__, &Map.put(&1, :usages, &1.usages ++ [resource_data]))
+    IO.puts(resource_data)
+
+    :timer.sleep(1000)
+    tick()
+  end
+
+  defp get_resource_usage() do
+    pad = &(&1 |> Integer.to_string() |> String.pad_leading(2, "0"))
+
+    timestamp = :os.system_time(:millisecond)
+    {memory_usage, memory_usage_unit} = memory_usage()
+    cpu_load = cpu_load()
+    {_, {h, m, s}} = :erlang.localtime()
+
+    %{
+      memory_usage: memory_usage,
+      memory_usage_unit: memory_usage_unit,
+      cpu_load: cpu_load,
+      local_time: "#{pad.(h)}:#{pad.(m)}:#{pad.(s)}",
+      timestamp: timestamp
+    }
+  end
+
+  defp cpu_load() do
+    # cpu load avg in a minute
+    ~s[cat /proc/loadavg]
     |> String.to_charlist()
     |> :os.cmd()
     |> List.to_string()
-    |> String.replace("\n", "")
+    |> String.split(" ")
+    |> List.first()
+  end
+
+  defp memory_usage() do
+    {:memory, size} =
+      :code_server
+      |> Process.whereis()
+      |> :erlang.process_info(:memory)
+
+    usage_in_mb = size / 1024 / 1024
+    {usage_in_mb, "MB"}
   end
 end
